ab361043f1950c97dc55528bb684d64a69b9a4f9
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-12-13 14:40:59 +0100

    u

--- setup.sh
+++ setup.sh
@@ -2,8 +2,12 @@
 
 ## bash ./nixos/setup.sh
 
-sudo rm /etc/nixos/configuration.nix
-sudo cp ./nixos/configuration.nix /etc/nixos/
+echo "Enter sudo password"
+read password
+
+
+echo $password | sudo rm /etc/nixos/configuration.nix
+echo $password | sudo cp ./nixos/configuration.nix /etc/nixos/
 
 export NIXPKGS_ALLOW_UNFREE=1
 nix-channel --update
@@ -14,11 +18,11 @@ nix-env -iA nixos.gnomeExtensions.material-shell
 # enable material shell
 gnome-extensions enable material-shell@papyelgringo
 
-sudo nixos-rebuild switch
+echo $password | sudo nixos-rebuild switch
 
 
 echo "plugins=(zsh-autosuggestions)" >> ~/.zshrc
-sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+echo $password | sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 echo "ZSH_THEME='powerlevel10k/powerlevel10k'" >> ~/.zshrc
 curl -L http://install.ohmyz.sh | sh
 echo "neofetch" >> ~/.zshrc
