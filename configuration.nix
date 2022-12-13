3325e9f82d6ac47cc7a6b5cfa773f3a8f335b9a9
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-12-13 13:13:00 +0100

    upd

--- configuration.nix
+++ configuration.nix
@@ -72,6 +72,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+  git
+  curl
   zsh
   zsh-autosuggestions
   zsh-syntax-highlighting
