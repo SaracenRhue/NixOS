0299d39d7b4582250d29200854b9dc581bbe254d
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-12-14 12:23:46 +0100

    u

--- configuration.nix
+++ configuration.nix
@@ -26,7 +26,7 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "Europe/Vienna";
+  time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -43,17 +43,44 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   # Configure keymap in X11
   services.xserver = {
     layout = "de";
     xkbVariant = "";
   };
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure console keymap
   console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.saracen = {
@@ -63,16 +90,12 @@
     packages = with pkgs; [];
   };
 
-  # Enable automatic login for the user.
-  # services.getty.autologinUser = "saracen";
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = (with pkgs; [
-  grub
   git
   curl
   zsh
@@ -84,6 +107,7 @@
   python311
   vscode
   github-desktop
+  firefox
   gparted
   krusader
   nextcloud-client
@@ -101,7 +125,7 @@
   cmatrix
   papirus-icon-theme
   ]) ++ (with pkgs.gnome; [
-    gnome-shell
+   #gnome-shell
     gnome-tweaks
     gnome-boxes
   ]) ++ (with pkgs.gnomeExtensions; [
