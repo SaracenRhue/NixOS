5bfcc1c44ea44d00548b390d01326334745d0148
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-12-16 14:12:47 +0100

    u

--- configuration.nix
+++ configuration.nix
@@ -14,9 +14,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.extraModulePackages = [ config.boot.kernelPackages.wireguard ];
+  # boot.extraModulePackages = [ config.boot.kernelPackages.wireguard ];
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
