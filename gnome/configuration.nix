29c4501b1bfe1aef14603ea2ffac75585706f1ac
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-04-05 18:48:44 +0200

    Update configuration.nix

--- gnome/configuration.nix
+++ gnome/configuration.nix
@@ -48,7 +48,9 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-    # Enable Tailscale
+  
+  # Enable Tailscale
+  networking.firewall.checkReversePath = "loose";
   services.tailscale.enable = true;
 
   # Configure keymap in X11
